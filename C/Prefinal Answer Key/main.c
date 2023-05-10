3864777fd073708e3be61f51dab437bc6042ed6b
zyguel <123135343+zyguel@users.noreply.github.com>
2023-05-10 18:47:50 +0800

    Update main.c

--- C/Prefinal Answer Key/main.c
+++ C/Prefinal Answer Key/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <stdbool.h> //Include this everytime you encouter BOOL;
 #define MAX_ITEM		10 
 #define STRING_LENGTH	30
 #define TRUE			1
